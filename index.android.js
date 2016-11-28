edbffa3e2b39f3176d00dde7c27aec4c25b04626
Haroon Khan <aitchkhan@gmail.com>
2016-11-28 18:28:41 +0500

    add image and custom field

--- index.android.js
+++ index.android.js
@@ -3,51 +3,33 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Text, Image, View } from 'react-native';
 
-export default class AwesomeProject extends Component {
-  render() {
+class Greetings extends Component {
+    render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+    <Text>Hello! {this.props.name}</Text>
+    );
+    }
+}
+
+class HelloWorldApp extends Component {
+  render() {
+  let pic = {
+  uri: 'http://192.168.1.70/buybye-platform/uploads/image-1480059523382.png'
+  };
+    return (
+      <View style={{alignItems:'center'}}>
+      <Image source={pic} style={{width:150, height: 150}}/>
+      <Greetings name="Haroon"/>
+      <Greetings name="Ayaz"/>
+      <Greetings name="Estes"/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+
+AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
