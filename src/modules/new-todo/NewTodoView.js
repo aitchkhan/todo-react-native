7fdc5280b8f38af44b4b429eb4da289c1062de3d
Haroon Khan <aitchkhan@gmail.com>
2017-01-25 20:09:04 +0500

    added assets

---
+++ src/modules/new-todo/NewTodoView.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import {
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Button,
+    ToolbarAndroid
+} from 'react-native';
+
+
+export default class NewTodoView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <Text>this is</Text>
+        );
+    }
+}
